4d926939109d513041daec648e29fee3ad47cc0b
Redeemm <71359434+Redeemm@users.noreply.github.com>
2023-06-21 15:03:51 +0000

    code update

--- yolov8_n_opencv.py
+++ yolov8_n_opencv.py
@@ -1,119 +1,120 @@
 import random
-import mediapipe as mp
-
 import cv2
-import numpy as np
 from ultralytics import YOLO
 
-# opening the file in read mode
-my_file = open("utils/coco.txt", "r")
-# reading the file
-data = my_file.read()
-# replacing end splitting the text | when newline ('\n') is seen.
-class_list = data.split("\n")
-my_file.close()
+def perform_object_detection():
+    # Function to read class data from file
+    def read_class_data(file_path):
+        class_list = []
+        with open(file_path, "r") as file:
+            data = file.read()
+            class_list = data.split("\n")
+        return class_list
 
-# print(class_list)
+    # Read shape class data
+    shape_class_list = read_class_data("utils/shapes.txt")
 
-# Generate random colors for class list
-detection_colors = []
-for i in range(len(class_list)):
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    detection_colors.append((b, g, r))
+    # Read gender class data
+    gender_class_list = read_class_data("utils/gender.txt")
 
-# load a pretrained YOLOv8n model
-model = YOLO("yolov8n-seg-custom.pt", "v8")
+    # Generate random colors for class list
+    detection_colors = []
+    for i in range(len(shape_class_list)):
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        detection_colors.append((b, g, r))
 
-# Vals to resize video frames | small frame optimise the run
-frame_wid = 640
-frame_hyt = 480
+    # Load a pretrained YOLOv8n shape_model
+    shape_model = YOLO("./utils/yolov8n-seg-custom-shape.pt", "v8")
+    gender_model = YOLO("./utils/yolov8n-seg-custom-gender.pt", "v8")
 
-cap = cv2.VideoCapture(0)
-# cap = cv2.VideoCapture("img.jpg")
+    # Vals to resize video frames | small frame optimise the run
+    frame_wid = 640
+    frame_hyt = 480
 
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
+    cap = cv2.VideoCapture(0)
+    # cap = cv2.VideoCapture("img.jpg")
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    # if frame is read correctly ret is True
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
 
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        # if frame is read correctly ret is True
 
-    #  resize the frame | small frame optimize the run
-    # frame = cv2.resize(frame, (frame_wid, frame_hyt))
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
 
-    # Predict on image
-    detect_params = model.predict(source=[frame], conf=0.45, save=False)
+        #  resize the frame | small frame optimize the run
+        # frame = cv2.resize(frame, (frame_wid, frame_hyt))
 
-    # Convert tensor array to numpy
-    DP = detect_params[0].numpy()
+        # Predict on image
+        shape_detect_params = shape_model.predict(source=[frame], conf=0.45, save=False)
+        gender_detect_params = gender_model.predict(source=[frame], conf=0.45, save=False)
 
-    if len(DP) != 0:
-        for j in range(len(detect_params[0])):
-            boxes = detect_params[0].boxes
-            box = boxes[j]
-            clsID = box.cls.numpy()[0]
-            cs = class_list[int(clsID)]
-            print(DP)
-            print(cs)
+        # Convert tensor array to numpy
+        SDP = shape_detect_params[0].numpy()
+        GDP = gender_detect_params[0].numpy()
 
+        if len(SDP) != 0 and len(GDP) != 0:
+            for j in range(len(shape_detect_params[0])):
+                for k in range(len(gender_detect_params[0])):
+                    boxes = shape_detect_params[0].boxes
+                    box = boxes[j]
+                    s_clsID = box.cls.numpy()[0]
+                    s_cs = shape_class_list[int(s_clsID)]
 
+                    g_boxes = gender_detect_params[0].boxes
+                    g_box = g_boxes[k]
+                    g_clsID = g_box.cls.numpy()[0]
+                    g_cs = gender_class_list[int(g_clsID)]
 
-    if len(DP) != 0:
-        for i in range(len(detect_params[0])):
-            # print(i)
+                    # print(g_cs, s_cs)
 
-            boxes = detect_params[0].boxes
-            box = boxes[i]  # returns one box
-            clsID = box.cls.numpy()[0]
-            conf = box.conf.numpy()[0]
-            bb = box.xyxy.numpy()[0]
+        if len(SDP) != 0:
+            for i in range(len(shape_detect_params[0])):
+                # print(i)
 
+                boxes = shape_detect_params[0].boxes
+                box = boxes[i]  # returns one box
+                clsID = box.cls.numpy()[0]
+                conf = box.conf.numpy()[0]
+                bb = box.xyxy.numpy()[0]
 
-    if len(DP) != 0:
-        for i in range(len(detect_params[0])):
-            # print(i)
+                cv2.rectangle(
+                    frame,
+                    (int(bb[0]), int(bb[1])),
+                    (int(bb[2]), int(bb[3])),
+                    detection_colors[int(clsID)],
+                    3,
+                )
 
-            boxes = detect_params[0].boxes
-            box = boxes[i]  # returns one box
-            clsID = box.cls.numpy()[0]
-            conf = box.conf.numpy()[0]
-            bb = box.xyxy.numpy()[0]
+                # Display class name and confidence
+                font = cv2.FONT_HERSHEY_COMPLEX
+                cv2.putText(
+                    frame,
+                    gender_class_list[int(g_clsID)] + "_" + shape_class_list[int(clsID)] + " " + str(round(conf, 3)) + "%",
+                    (int(bb[0]), int(bb[1]) - 10),
+                    font,
+                    1,
+                    (255, 255, 255),
+                    2,
+                )
 
-            cv2.rectangle(
-                frame,
-                (int(bb[0]), int(bb[1])),
-                (int(bb[2]), int(bb[3])),
-                detection_colors[int(clsID)],
-                3,
-            )
+        # Display the resulting frame
+        cv2.imshow("ObjectDetection", frame)
 
-            # Display class name and confidence
-            font = cv2.FONT_HERSHEY_COMPLEX
-            cv2.putText(
-                frame,
-                class_list[int(clsID)] + " " + str(round(conf, 3)) + "%",
-                (int(bb[0]), int(bb[1]) - 10),
-                font,
-                1,
-                (255, 255, 255),
-                2,
-            )
+        # Terminate run when "Q" pressed
+        if cv2.waitKey(1) == ord("q"):
+            break
 
-    # Display the resulting frame
-    cv2.imshow("ObjectDetection", frame)
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
 
-    # Terminate run when "Q" pressed
-    if cv2.waitKey(1) == ord("q"):
-        break
-
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+# Call the function to start object detection
+perform_object_detection()
