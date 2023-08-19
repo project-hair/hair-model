3f2b5e3c81165cda7249f34442b279f112d45887
Redeemm <71359434+Redeemm@users.noreply.github.com>
2023-08-19 19:19:26 +0000

    Db and model

---
+++ db_config.py
@@ -0,0 +1,8 @@
+db_config = {
+    'user': 'root',
+    'password': 'redeem419',
+    'host': 'localhost',
+    'port': '3307',
+    'database': 'hair_db',
+    'raise_on_warnings': True
+}
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
 from video_detect import VideoDetector
 from image_detect import ImageDetector
+from db_config import db_config
+import mysql.connector
 import cv2
 import numpy as np
 
@@ -8,6 +10,7 @@ app = Flask(__name__)
 
 image_detector = ImageDetector()
 video_detector = VideoDetector()
+
 
 @app.route('/image_detection', methods=['POST'])
 def perform_image_detection():
@@ -20,14 +23,71 @@ def perform_image_detection():
     results = image_detector.detect_objects(image)
 
     if len(results) == 0:
-        return jsonify({"error": "Image does not contain any detected objects. Kindly select a clearer image for processing"}), 400
+        return jsonify({"error": "Image does not contain any detected objects. Kindly select a clearer image for processing"}), 500
 
     return jsonify(results)
+
 
 
 @app.route('/video_detection', methods=['GET'])
 def perform_video_detection():
     return video_detector.process_frame()
+
+
+
+@app.route('/api/all-products/', methods=['GET'])
+def get_data():
+    cnx = mysql.connector.connect(**db_config)
+    cursor = cnx.cursor()
+
+    query = 'SELECT * FROM hair'
+    cursor.execute(query)
+
+    data = cursor.fetchall()
+    cursor.close()
+    cnx.close()
+    result = []
+
+    for row in data:
+        result.append({
+            'hairId': row[0],
+            'gender': row[1],
+            'shape': row[2],
+            'url': row[3],
+        })
+    return jsonify(result)
+
+
+
+@app.route('/api/filter/', methods=['POST'])
+def filter():
+    data = request.json
+    gender = data.get('gender')
+    shape = data.get('shape')
+
+    if not gender or not shape:
+        return jsonify({'error': 'Both gender and shape parameters are required.'}), 400
+
+    cnx = mysql.connector.connect(**db_config)
+    cursor = cnx.cursor()
+    query = 'SELECT * FROM hair WHERE hair.gender = %s AND hair.shape = %s'
+    cursor.execute(query, (gender, shape))
+    data = cursor.fetchall()
+
+    cursor.close()
+    cnx.close()
+    result = []
+
+    for row in data:
+        result.append({
+            'hairId': row[0],
+            'gender': row[1],
+            'shape': row[2],
+            'url': row[3],
+        })
+    return jsonify(result)
+
+
 
 
 if __name__ == '__main__':
